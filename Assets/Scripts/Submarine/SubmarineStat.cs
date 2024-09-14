b939127244f6a6b9b69c59f303323cdbc2f0a42f
htamlive <nghichmaychoi@gmail.com>
2024-09-14 23:06:31 +0700

    Revert "remove some redundants"
    This reverts commit bb7b7d7d3967c5b79a13a5d4607d5ae626479d9a.

--- Assets/Scripts/Submarine/SubmarineStat.cs
+++ Assets/Scripts/Submarine/SubmarineStat.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 
 [RequireComponent(typeof(RecoveryCounter))]
 public class SubmarineStat : MonoBehaviour
