74cdd503bec9bb5e89430e635f1e2f8c15030bb4
htamlive <nghichmaychoi@gmail.com>
2024-09-19 20:38:37 +0700

    update pause screen trigger

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,9 +42,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
         {
-            pauseMenu.SetActive(true);
+            var status = pauseMenu.gameObject.activeInHierarchy;
+            pauseMenu.SetActive(!status);
         }
         if (Input.GetKeyDown(KeyCode.Print) && !Utilities.CheckPlatform(RuntimePlatform.Android) && !Utilities.CheckPlatform(RuntimePlatform.WebGLPlayer))
         {
